339038982c928b2c11ac48dd692c60943fa11ba8
Stain <ytb.stainmc2102@gmail.com>
2026-01-01 10:39:50 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const CONFIG = {
+    TEST_MODE: false, // Default should be false
+    TARGET_TIME: new Date('2026-02-17T00:00:00'), // Lunar New Year 2026 (Year of the Horse)
+    MUSIC_PATH: 'assets/nhactet.mp3', // Tet music
+    YOUTUBE_URL: 'https://www.youtube.com/embed/SPC6YA0K3-4?autoplay=1&loop=1&playlist=SPC6YA0K3-4&enablejsapi=1', // Added enablejsapi for control
+};
